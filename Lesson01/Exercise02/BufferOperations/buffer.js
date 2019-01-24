43f9f0cd89d216b7a298c6f6fabb95dd76304186
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-01-24 18:09:36 +0530

    Add files via upload

---
+++ Lesson01/Exercise02/BufferOperations/buffer.js
@@ -0,0 +1,36 @@
+// 1 . Create an instance of buffer class using alloc()
+
+varbuf1 = Buffer.alloc(5);
+var buf2 = new Buffer(5);
+
+console.log(buf1)
+console.log(buf2)
+
+// 2. Create a Buffer from a given array using from() 
+
+var buf3 = new Buffer([10, 20, 30, 40, 50]);
+var buf4 = Buffer.from([ 10, 20, 30, 40, 50]);
+
+console.log(buf3)
+console.log(buf4) 
+
+// 3. Create a buffer from a given string and optionally encoding type using from() or from an instance of a buffer
+ 
+
+var buf5 = new Buffer("Hi packt students!", "utf-8");
+var buf6 = Buffer.from("Hi packt students!", "utf-8")
+
+console.log(buf5)
+console.log(buf6)
+
+// 4. Write into a buffer
+// buffer.write(string[, offset][, length][, encoding])
+
+len = buf5.write("Pactk student", "utf-8")
+
+console.log (len)
+
+// 5. Read from buffer
+//buffer.toString([encoding][, start][, end]
+
+console.log(buf5.toString("utf-8", 0, 13))
