0f4915780692cbb9962864192986ea153b734483
TrainingByPackt <maheshd@packt.com>
2019-02-22 18:50:20 +0530

    Folders added

--- Lesson06/Activity15/Blogging Application/blog/src/app/service/auth.service.ts
+++ Lesson06/Activity15/Blogging Application/blog/src/app/service/auth.service.ts
@@ -2,40 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router'
 import { map } from 'rxjs/operators';
-<<<<<<< HEAD
-=======
-import { environment } from '../../environments/environment'
->>>>>>> 8c0a98fe1daf3902bd0d351326c6815b0ca274cc
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-<<<<<<< HEAD
 
   registerUrl= "http://localhost:3000/auth/register";
   loginUrl= "http://localhost:3000/auth/sign_in";
-=======
-  config = environment;
->>>>>>> 8c0a98fe1daf3902bd0d351326c6815b0ca274cc
 
   constructor(private http: HttpClient,
     private router: Router) { }
 
   registerUser(user) {
-<<<<<<< HEAD
     return this.http.post<any>(`${this.registerUrl}`, user)
   }
 
   loginUser(user) {
     return this.http.post<any>(`${this.loginUrl}`, { 'email': user.email, 'password': user.password })
-=======
-    return this.http.post<any>(`${this.config.registerUrl}`, user)
-  }
-
-  loginUser(user) {
-    return this.http.post<any>(`${this.config.loginUrl}`, { 'email': user.email, 'password': user.password })
->>>>>>> 8c0a98fe1daf3902bd0d351326c6815b0ca274cc
       .pipe(map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
---
+++ Lesson06/Activity17/Blogging Application/blog/e2e/src/outer-link-directive-stub.ts
@@ -0,0 +1,13 @@
+import { Component, Directive,OnInit,Input,HostListener } from '@angular/core';
+@Directive({
+    selector: '[routerLink]'
+  })
+  export class RouterLinkDirectiveStub {
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+  
+    @HostListener('click')
+    onClick() {
+      this.navigatedTo = this.linkParams;
+    }
+  }
